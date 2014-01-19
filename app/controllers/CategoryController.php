4189f80e40b716ec5bbaa321c1b81e8777826a11
Luish000 <luish000.lh@gmail.com>
2014-01-19 16:25:00 -0430

    se quitaron todos los url encode para poder hacer cambios que se comentaron en el anterior commit

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -22,7 +22,7 @@ class CategoryController extends BaseController {
     public function newCategory() {
         if (ValidationManager::isValid(Input::all(),unserialize(NEW_CATEGORY_RULES))) {
             $category = new Category();
-            $category->nombre = urlencode(Input::get('titulo'));
+            $category->nombre = Input::get('titulo');
             $category->id_categoria_padre = Input::get('categoria_padre');
             $category->url_imagen = FileManager::moveFile(Input::file('imagen'),CATEGORY_PATH.$category->nombre);
             $category->url_video = FileManager::moveFile(Input::file('video'),CATEGORY_PATH.$category->nombre);
